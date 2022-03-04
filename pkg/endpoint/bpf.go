fdc983e9dd60ffecd5cfb7a9bdb1462cbf7d76f3
Varun Marupadi <varunmar@google.com>
2022-03-04 14:07:21 -0800

    Update the local redirect maps when pods with known labels are created/modified
    Change-Id: Ib20968181bd66bfe53979f8e9cc2a22c5e9ed6f0

--- pkg/endpoint/bpf.go
+++ pkg/endpoint/bpf.go
@@ -1019,6 +1019,9 @@ func (e *Endpoint) deleteMaps() []error {
 		}
 	}
 
+	// Remove localredirect map entries on endpoint delete as well.
+	errors = append(errors, e.DeleteLocalRedirectMapIfNecessary())
+
 	return errors
 }
 
