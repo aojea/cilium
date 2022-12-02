9f779d843098483edf29703b419449916e2f46dd
Rahul Joshi <rkjoshi@google.com>
2022-12-02 15:42:09 -0500

    Add option to disable policy_event_count metric.
    In clusters with large churn of pods, the metric can OOM metrics
    exporter. This is because it creates a new stream for each pod, but
    doesn't quickly garbage collect when they're deleted.
    
    Bug: 242069352
    Change-Id: I0bf0bbf535a1e08fa7365d6abbfee2ba9d22ccb0
    (cherry picked from commit 1b027693d4f3b24bdc086d03f649635d114bfc00)
    Reviewed-on: https://gke-internal-review.googlesource.com/c/third_party/cilium/+/583396
    Reviewed-by: Satish Matti <smatti@google.com>
    Unit-Verified: Prow_Bot_V2 <425329972751-compute@developer.gserviceaccount.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -243,6 +243,9 @@ const (
 	// EnableHostFirewall enables network policies for the host
 	EnableHostFirewall = "enable-host-firewall"
 
+	// DisablePolicyEventCountMetric  disables the policy event count metric on this host.
+	DisablePolicyEventCountMetric = "disable-policy-event-count-metric"
+
 	// EnableRedirectService enables google redirect service for the host
 	EnableRedirectService = "enable-redirect-service"
 
@@ -2019,6 +2022,9 @@ type DaemonConfig struct {
 	// EnableHostFirewall enables network policies for the host
 	EnableHostFirewall bool
 
+	// DisablePolicyEventCountMetric  disables the policy event count metric on this host.
+	DisablePolicyEventCountMetric bool
+
 	// EnableRedirectService enables google redirect service for the host
 	EnableRedirectService bool
 
@@ -2983,6 +2989,7 @@ func (c *DaemonConfig) Populate() {
 	c.EnableMKE = viper.GetBool(EnableMKE)
 	c.CgroupPathMKE = viper.GetString(CgroupPathMKE)
 	c.EnableHostFirewall = viper.GetBool(EnableHostFirewall)
+	c.DisablePolicyEventCountMetric = viper.GetBool(DisablePolicyEventCountMetric)
 	c.EnableRedirectService = viper.GetBool(EnableRedirectService)
 	c.EnableFQDNNetworkPolicy = viper.GetBool(EnableFQDNNetworkPolicy)
 	c.EnableTrafficSteering = viper.GetBool(EnableTrafficSteering)
